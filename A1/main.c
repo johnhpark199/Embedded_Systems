f6e0c13976fe6a5dd77b2996d1866ee4964b23ce
johnhpark199 <137963760+johnhpark199@users.noreply.github.com>
2023-11-19 23:06:01 -0800

    Update main.c

--- A1/main.c
+++ A1/main.c
@@ -1,65 +1,27 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
+/*
+ ******************************************************************************
+* file      : main.c
+* project   : EE 329 F'23 A1
+* details   : LEDs binary counting timer
+* authors   : John Park
+*           :
+* version   : 0.1
+* date      : 2023-NOV-14
+ * compiler  : STM32CubeIDE Version: 1.13.1 (2023)
+* target    : NUCLEO-L4A6ZG
+ ******************************************************************************
+ * revisions :
+* 2023-NOV-14 created.
+******************************************************************************
+* origins   :
+* modified from CubeIDE auto-generated main.c  (c) 2023 STMicro.
+* modified from EE329 Lab Manual Sample Code
+******************************************************************************
+
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-/* USER CODE BEGIN PFP */
-
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
 
 void main(void) {
 
